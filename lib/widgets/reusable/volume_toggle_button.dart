1da0e370f64f6cd07977569d64068a410609eb0b
JesbinJose <jesbinjose57@gmail.com>
2024-01-19 05:27:11 +0530

    made volume button reusable

--- lib/widgets/reusable/volume_toggle_button.dart
+++ lib/widgets/reusable/volume_toggle_button.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:play_video/models/state.dart';
 
@@ -6,24 +5,22 @@ class DefaultMuteToggleButton extends StatelessWidget {
   const DefaultMuteToggleButton({
     super.key,
     required this.state,
+    this.muteWidget = const Icon(Icons.volume_off),
+    this.unMuteWidget = const Icon(Icons.volume_up),
   });
 
   final VideoPlayerState state;
+  final Widget muteWidget;
+  final Widget unMuteWidget;
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-      valueListenable:
-          state.volume.muteNotifier,
+      valueListenable: state.volume.muteNotifier,
       builder: (_, value, __) {
         return IconButton(
-          onPressed: () =>
-              state.volume.toggleMute(),
-          icon: Icon(
-            value
-                ? Icons.volume_off
-                : Icons.volume_up,
-          ),
+          onPressed: () => state.volume.toggleMute(),
+          icon: value ? muteWidget : unMuteWidget,
         );
       },
     );
