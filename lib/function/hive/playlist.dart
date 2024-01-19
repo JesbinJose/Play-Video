ff7ae63aa6a4359c9a80d65e9fe06ed8791f3d54
JesbinJose <jesbinjose57@gmail.com>
2024-01-19 07:51:03 +0530

    playlist functions updated

--- lib/function/hive/playlist.dart
+++ lib/function/hive/playlist.dart
@@ -1,27 +1,28 @@
 import 'package:hive/hive.dart';
 
 class PlayList {
-  static Box? box;
+  PlayList() {
+    loadDB();
+  }
+  Box? box;
   //it is for loading data base
-  static Future<void> loadDB() async {
+  Future<void> loadDB() async {
     box ??= await Hive.openBox('playlist_db');
   }
 
   //its for create playlist
-  static Future<void> addPlaylist(String playlistName, {List<String>? list}) async {
-    loadDB();
+  Future<void> addPlaylist(String playlistName,
+      {List<String>? list}) async {
     await box!.put(playlistName, list ?? []);
   }
 
   //delete playlist
-  static Future<void> deletePlaylist(String playlistName) async {
-    loadDB();
+  Future<void> deletePlaylist(String playlistName) async {
     await box!.delete(playlistName);
   }
 
   //add video to playlist
-  static Future<void> addVideo(String path, String playlistName) async {
-    loadDB();
+  Future<void> addVideo(String path, String playlistName) async {
     final List<dynamic> list = box!.get(playlistName);
     if (!list.contains(path)) {
       list.add(path);
@@ -30,27 +31,24 @@ class PlayList {
   }
 
   //remove video
-  static Future<void> removeVideo(String path, String playlistaName) async {
-    loadDB();
+  Future<void> removeVideo(String path, String playlistaName) async {
     final List<dynamic> list = box!.get(playlistaName);
     list.remove(path);
     await box!.put(playlistaName, list);
   }
 
   //get full data base as the playlist name and the videos in the playlist
-  static Map<String, List<String>> getFullPlaylist() {
-    loadDB();
+  Map<String, List<String>> getFullPlaylist() {
     final List list = box!.keys.toList();
     Map<String, List<String>> playlists = {};
     for (String playlistName in list) {
       //getting single by single whole playlists
-      playlists[playlistName] =box!.get(playlistName);
+      playlists[playlistName] = box!.get(playlistName);
     }
     return playlists;
   }
 
-  static Future<void> deleteAVideoFromAllDataBase(String path) async {
-    loadDB();
+  Future<void> deleteAVideoFromAllDataBase(String path) async {
     for (String playlistName in box!.keys) {
       List list = box!.get(playlistName);
       list.remove(path);
